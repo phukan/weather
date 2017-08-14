3b634dec1d0002bc6d67c6345e62baaf2689f295
phukan <ujjalphukan@gmail.com>
2017-08-14 13:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,10 +97,10 @@ def makeWebhookResultForGetAtomicNumber(data):
 	
 def makeYqlQuery(req):
 	result = req.get("result")
-		parameters = result.get("parameters")
-		city = parameters.get("geo-city")
-		if city is None:
-			return None
+	parameters = result.get("parameters")
+	city = parameters.get("geo-city")
+	if city is None:
+		return None
 
 	return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 	
